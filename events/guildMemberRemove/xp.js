fc5874f8a1f34695ef5b00808dcdbc55efd195f4
Prince527 <75485262+Prince527GitHub@users.noreply.github.com>
2022-12-20 14:00:30 -0400

    Update xp.js

--- events/guildMemberRemove/xp.js
+++ events/guildMemberRemove/xp.js
@@ -2,7 +2,7 @@ const client = require("../../index");
 const Levels = require("discord-xp");
 
 module.exports = async(member) => {
-    if (guildMemberRemove.bot) return;
+    if (member.bot) return;
 
     Levels.deleteUser(member.id, member.guild.id);
 }
