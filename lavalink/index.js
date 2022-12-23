79f6f34ae6ba2aa445cdae3456f293e033be791b
Prince527 <75485262+Prince527GitHub@users.noreply.github.com>
2022-12-23 08:26:57 -0400

    Added debug to built in lavalink server

--- lavalink/index.js
+++ lavalink/index.js
@@ -2,12 +2,14 @@ const config = require("../settings/settings.json");
 const colors = require("../assets/api/console");
 const { spawn } = require('child_process');
 
-if (config.music.server === false) return;
+if (config.music.server.enabled === false) return;
 
 const child = spawn("java", ["-jar", `${process.cwd()}/lavalink/app/Lavalink.jar`], { cwd: `${process.cwd()}/lavalink/app` });
 
 child.stdout.on('data', (data) => {
     const stringData = data.toString();
+
+    if (config.music.server.debug) console.log(stringData);
 
     if (stringData.includes("Lavalink is ready to accept connections.")) return global.lavalinkServer.success = `${colors.fgWhite("Lavalink Server:")} ${colors.fgGreen("√")}`;
     else global.lavalinkServer.success = `${colors.fgWhite("Lavalink Server:")} ${colors.fgRed("×")}`;
