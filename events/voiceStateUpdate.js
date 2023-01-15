18a3544bd48c3cbcb42bdd24dff0701bf591c866
Prince527 <75485262+Prince527GitHub@users.noreply.github.com>
2023-01-15 14:05:32 -0400

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,6 +2,8 @@ const { Events } = require("discord.js");
 const client = require("../index");
 
 client.on(Events.VoiceStateUpdate, (oldVoice, newVoice) => {
+    if (!client.config.music.enabled) return;
+
     const player = client.poru.players.get(oldVoice.guild.id);
     if (!player) return;
 
