2948b8c90bab49c632729405419a742e9ca8e52b
Prince527 <75485262+Prince527GitHub@users.noreply.github.com>
2023-05-02 19:17:18 -0300

    Experimental vote skip system

--- SlashCommands/music/skip.js
+++ SlashCommands/music/skip.js
@@ -1,8 +1,6 @@
-const {
-    EmbedBuilder,
-    ApplicationCommandType,
-    ApplicationCommandOptionType
-} = require("discord.js");
+const { EmbedBuilder, ApplicationCommandType, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const musicSchema = require("../../models/server/music.js");
+const { getRandomInt } = require("../../assets/api/crypto");
 
 module.exports = {
     name: "skip",
@@ -45,35 +43,112 @@ module.exports = {
             ]
         });
 
-        // const members = channel.members;
+        function skipAccept(vote) {
+            player.stop();
 
-        // members.forEach(member => {
-        //     if (member.user.bot) return;
+            if (vote) interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                    .setDescription("🟩 **|** Most people voted to skip")
+                    .setColor("Green")
+                ]
+            });
 
-        //     console.log(member.user.username);
-        // });
+            return interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                    .setDescription("⏭ **|** Skipped **current** song")
+                    .setColor("Blue")
+                ]
+            });
+        }
 
-        // if (player.playing) return skip();
-        // else if (player.paused) return skip();
-        // else return interaction.followUp({
-        //     embeds: [
-        //         new EmbedBuilder()
-        //         .setDescription("There is nothing playing")
-        //         .setColor("Yellow")
-        //     ]
-        // });
+        function skipDeny() {
+            return interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                    .setDescription("🟥 **|** The majority voted not to skip")
+                    .setColor("Red")
+                ]
+            });
+        }
 
-        // function skip() {
-        player.stop();
+        const commandType = (await musicSchema.findOne({ Guild: interaction.guild.id })).Skip === true ? true : false;
 
-        interaction.followUp({
-            embeds: [
-                new EmbedBuilder()
-                .setDescription("⏭ **|** Skipped **current** song")
-                .setColor("Blue")
-            ]
-        });
-        // }
+        if (commandType) {
+            const members = channel.members;
+
+            const userIds = await members.filter(member => !member.user.bot).map(member => member.user.id);
+
+            const message = await interaction.followUp({
+                embeds: [
+                    new EmbedBuilder()
+                    .setDescription("🟦 **|** Voting to skip **current** song")
+                    .setFooter({ text: "(voting only last for 30 seconds)" })
+                    .setColor("Blue")
+                ],
+                components: [
+                    new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                        .setCustomId(`acceptSkip-${interaction.guild.id}`)
+                        .setLabel('Accept')
+                        .setStyle(ButtonStyle.Success),
+                        new ButtonBuilder()
+                        .setCustomId(`denySkip-${interaction.guild.id}`)
+                        .setLabel('Deny')
+                        .setStyle(ButtonStyle.Danger),
+                    )
+                ],
+            });
+
+            let results = null;
+            let users = userIds;
+
+            const filter = (i) => userIds.includes(interaction.user.id);
+
+            const voteCollector = await message.createMessageComponentCollector({ filter: filter, time: 30000 });
+
+            voteCollector.on('collect', async (button) => {
+                if (!users.includes(button.user.id)) return button.reply({ content: "You have already voted", ephemeral: true });
+
+                if (results === null) results = -1 * userIds;
+
+                const index = users.indexOf(button.user.id);
+                if (index > -1) users.splice(index, 1);
+
+                if (button.customId === `acceptSkip-${interaction.guild.id}`) results++;
+                else if (button.customId === `denySkip-${interaction.guild.id}`) results--;
+
+                return button.reply({ content: "Vote submitted", ephemeral: true });
+            });
+
+            voteCollector.on('end', async (msgs, reason) => {
+                if (!results === null) return interaction.followUp({
+                    embeds: [
+                        new EmbedBuilder()
+                        .setDescription("🟨 **|** Canceling did not receive any votes")
+                        .setColor("Yellow")
+                    ]
+                });
+
+                if (userIds.length === 0 || userIds.length < results / 2 || results > 0) return skipAccept(true);
+                else if (results === 0) {
+                    const chance = getRandomInt(1 , 2);
+
+                    interaction.followUp({
+                        embeds: [
+                            new EmbedBuilder()
+                            .setDescription("⬜ **|** We're flipping a coin to break the tie.")
+                            .setColor("White")
+                        ]
+                    });
+
+                    if (chance === 1) return skipAccept(true)
+                    else return skipDeny();
+                } else return skipDeny();
+            });
+        } else return skipAccept();
 
     },
 };
---
+++ models/server/music.js
@@ -0,0 +1,6 @@
+const { Schema, model } = require('mongoose');
+
+module.exports = model('music', new Schema({
+    Guild: String,
+    Skip: Boolean,
+}));
