55c6c1c83abf0d412c22a57ed028ec129474a672
Prince527 <Prince527gaming@gmail.com>
2024-09-23 16:14:23 -0300

    Update modlogs.js

--- commands/utils/options/modlogs.js
+++ commands/utils/options/modlogs.js
@@ -1,4 +1,4 @@
-const { ActionRowBuilder, ButtonBuilder } = require("discord.js");
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
 const modlogSchema = require("../../../models/logs/modlogs");
 
 module.exports = {
